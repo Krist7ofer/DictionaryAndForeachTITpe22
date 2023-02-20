24ecc8f144d1441500c346fbe5794b5a519ebc4b
opilane <opilane@HP-CZC0359NNZ>
2023-02-20 13:03:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DictionaryAndForeachTITpe22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tänavad = new Dictionary<int, string>()
+            {
+                {1, "Turu tänav" },
+                {2, "Pikk tänav" },
+                {3, "Kespäeva tänav" }
+            };
+            foreach (var street in tänavad)
+            {
+                Console.WriteLine($"{street.Key} - {street.Value}");
+            }
+        }
+    }
+}
